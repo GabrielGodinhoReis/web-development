5c63d5862c003f3d90c440b88f1c2d227d38419f
Gabriel Godinho <gabrielgodi82@gmail.com>
2026-08-26 16:45:33 -0300

    Organiza arquivos e adiciona Web 04

---
+++ web04/src/app/cesta-compras/cesta-compras.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cesta-compras',
+  styleUrl: './cesta-compras.css',
+  templateUrl: './cesta-compras.html',
+})
+export class CestaCompras {}
